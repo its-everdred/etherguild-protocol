71716b64d450547ec28263ca0dd86c0ea6327f6f
Abhinil Agarwal <abhijaipur2011@gmail.com>
2025-02-07 01:26:42 +0530

    initial commit

---
+++ src/QuestDonation.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract QuestDonations is Ownable {
+    using SafeERC20 for IERC20;
+
+    address public multisig;
+    AggregatorV3Interface public priceOracle; // ETH/USD price feed
+    uint256 public constant MAX_DONATION = 5000 * 1e18; // $5000 worth
+    uint256 public constant YEAR = 365 days;
+
+    mapping(address => uint256) public yearlyDonations;
+    mapping(address => uint256) public lastDonationTimestamp;
+    mapping(address => bool) public allowedTokens;
+
+    event DonationReceived(address indexed donor, address indexed token, uint256 amount, uint256 usdValue);
+    event TokenAllowed(address token, bool status);
+    event FundsWithdrawn(address indexed token, uint256 amount);
+
+    constructor(address _multisig, address _priceOracle) {
+        require(_multisig != address(0), "Invalid multisig address");
+        require(_priceOracle != address(0), "Invalid oracle address");
+        
+        multisig = _multisig;
+        priceOracle = AggregatorV3Interface(_priceOracle);
+    }
+
+    modifier withinDonationLimit(uint256 ethAmount) {
+        (, int256 price, , , ) = priceOracle.latestRoundData();
+        require(price > 0, "Invalid price");
+        uint256 usdValue = (ethAmount * uint256(price)) / 1e8; // Convert to USD
+        
+        uint256 lastYear = lastDonationTimestamp[msg.sender];
+        if (block.timestamp - lastYear > YEAR) {
+            yearlyDonations[msg.sender] = 0;
+            lastDonationTimestamp[msg.sender] = block.timestamp;
+        }
+        require(yearlyDonations[msg.sender] + usdValue <= MAX_DONATION, "Donation exceeds $5000/year. Contact info@etherguild.xyz");
+        
+        yearlyDonations[msg.sender] += usdValue;
+        _;
+    }
+
+    function donateETH() external payable withinDonationLimit(msg.value) {
+        emit DonationReceived(msg.sender, address(0), msg.value, msg.value * uint256(priceOracle.latestAnswer()) / 1e8);
+    }
+
+    function donateERC20(address token, uint256 amount) external withinDonationLimit(amount) {
+        require(allowedTokens[token], "Token not allowed");
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        emit DonationReceived(msg.sender, token, amount, amount);
+    }
+
+    function allowToken(address token, bool status) external onlyOwner {
+        allowedTokens[token] = status;
+        emit TokenAllowed(token, status);
+    }
+
+    function withdraw(address token, uint256 amount) external {
+        require(msg.sender == multisig, "Only multisig can withdraw");
+        if (token == address(0)) {
+            payable(multisig).transfer(amount);
+        } else {
+            IERC20(token).safeTransfer(multisig, amount);
+        }
+        emit FundsWithdrawn(token, amount);
+    }
+}
